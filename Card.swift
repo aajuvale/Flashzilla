fb5a5a45a3c0549eb726e8598bdd17fb812cbf44
aajuvale <ahmed.juvale@gmail.com>
2025-08-19 12:10:32 -0700

    feat: designed a single card view

---
+++ Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Ahmed Juvale on 8/19/25.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the thirteenth doctor in doctor who?", answer: "Jodie Whittaker")
+}
--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,65 +6,10 @@
 //
 
 import SwiftUI
-import Combine
-
-
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
-    }
-}
 
 struct ContentView: View {
-    // Color blindness Accesibility mode
-//    @Environment(\.accessibilityDifferentiateWithoutColor)
-//    var accessibilityDifferentiateWithoutColor
-//
-//    var body: some View {
-//        HStack {
-//            if accessibilityDifferentiateWithoutColor {
-//                Image(systemName: "checkmark.circle")
-//            }
-//
-//            Text("Success")
-//        }
-//        .padding()
-//        .background(accessibilityDifferentiateWithoutColor ? .black : .green)
-//        .foregroundStyle(.white)
-//        .clipShape(.capsule)
-//    }
-
-    // Reduce motion
-//    @Environment(\.accessibilityReduceMotion)
-//    var accessibilityReduceMotion
-//
-//    @State
-//    private var scale = 1.0
-//
-//    var body: some View {
-//        Button("Hello, world!") {
-//            withOptionalAnimation {
-//                scale *= 1.5
-//            }
-//        }
-//        .scaleEffect(scale)
-//    }
-
-    // Reduce transparency
-    @Environment(\.accessibilityReduceTransparency)
-    var accessibilityReduceTransparency
-
-    @State
-    private var scale = 1.0
-
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .background(accessibilityReduceTransparency ? .black : .black.opacity(0.5))
-            .foregroundStyle(.white)
-            .clipShape(.capsule)
+
     }
 }
 
