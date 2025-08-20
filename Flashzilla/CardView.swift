c4a88ebca634b9cf0df09d48546ecbc528afdeda
aajuvale <ahmed.juvale@gmail.com>
2025-08-19 17:10:17 -0700

    feat: implemented colored views w/ swiping

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor)
+    var accessibilityDifferentiateWithoutColor
     @State
     private var offset = CGSize.zero
 
@@ -21,7 +23,17 @@ struct CardView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+                .fill(
+                    accessibilityDifferentiateWithoutColor ?
+                        .white :
+                        .white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+                )
+                .background(
+                    accessibilityDifferentiateWithoutColor ? nil :
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
 
             VStack {
