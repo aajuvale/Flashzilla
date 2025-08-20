2387891e07edf964d7a2ad6dbfea0290a51423c9
aajuvale <ahmed.juvale@gmail.com>
2025-08-19 18:26:50 -0700

    feat: implemented timer countdown

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 extension View {
     func stacked(at position: Int, in total: Int) -> some View {
@@ -15,11 +16,21 @@ extension View {
 }
 
 struct ContentView: View {
+    @Environment(\.scenePhase)
+    var scenePhase
+
     @Environment(\.accessibilityDifferentiateWithoutColor)
     var accessibilityDifferentiateWithoutColor
 
     @State
+    private var isActive = true
+
+    @State
     private var cards = Array<Card>(repeating: .example, count: 10)
+
+    @State
+    private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         ZStack {
@@ -28,6 +39,14 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -61,6 +80,20 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .padding()
                 }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
