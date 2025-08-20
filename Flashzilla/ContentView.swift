98028319d6c941f8b58fe588f75744bd574f425c
aajuvale <ahmed.juvale@gmail.com>
2025-08-19 18:34:05 -0700

    feat: implemented hit testing

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -57,6 +57,15 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
             }
 
             if accessibilityDifferentiateWithoutColor {
@@ -91,7 +100,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) {
             if scenePhase == .active {
-                isActive = true
+                if cards.isEmpty == false {
+                    isActive = true
+                }
             } else {
                 isActive = false
             }
@@ -100,5 +111,15 @@ struct ContentView: View {
 
     func removeCard(at index: Int) {
         cards.remove(at: index)
+
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
